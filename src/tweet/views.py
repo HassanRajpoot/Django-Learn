9d5555be528b8f2b2e8fde98dbacfb26f3a47a58
HassanRajpoot <xhah450@gmail.com>
2024-10-20 23:10:47 +0500

    Django Crash Course views post edit

--- src/tweet/views.py
+++ src/tweet/views.py
@@ -27,7 +27,12 @@ def tweet_create(request):
 def tweet_edit(request,tweet_id):
     tweet = get_object_or_404(Tweet,pk= tweet_id,user = request.user)
     if request.method == "POST":
-        pass
+        form = TweetForm(request.POST , request.FILES,instance=tweet)
+        if form.is_valid():
+            tweet = form.save(commit=False)
+            tweet.user = request.user
+            form.save()
+            return redirect('tweet_list')
     else:
         form = TweetForm(instance=tweet)
         return render(request,'tweet_form.html',{'form':form})
